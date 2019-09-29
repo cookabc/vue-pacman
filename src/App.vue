3ea154541b963daa20d68953f9df5814074aa497
chuangcius <chuangcius@gmail.com>
2019-09-29 18:11:38 +0800

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <PacMan />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import PacMan from './components/PacMan.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    PacMan,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/PacMan.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pac-man">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PacMan extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+.pac-man {
+  position: relative;
+}
+</style>
